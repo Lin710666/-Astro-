48559ca834d02695f0196cb16a18e56dfc245865
HikiLin <linxinze935@gmail.com>
2026-09-03 18:28:31 +0800

    finish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://hikisfirstastro.netlify.app/"
+  site: "https://hikisfirstastro.netlify.app/",
+  integrations: [preact()]
 });
